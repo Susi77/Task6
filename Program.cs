b211c38e2577d6327686cd179436b6182dc98d1d
Susanna Makaryan <69461145+Susi77@users.noreply.github.com>
2020-09-12 22:57:11 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ConnectToDB.DataAcces;
+using ConnectToDB.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ConnectToDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employdal employdal = new Employdal();
+
+            for (int i = 1; i < 10; i++)
+            {
+                employdal.InsertEmploy
+
+               (new Employ() { ID = i, Name = "Susi" + i, Age = 22, Salary = 500000 });
+                
+
+                Console.WriteLine($"ResultText for Employ {i}: {employdal.ResultText}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
